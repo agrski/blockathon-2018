e50575377c07eabeaff9ba291677db0df400f8e1
Alex Rakowski <alex.rakowski@thehutgroup.com>
2018-01-19 16:57:42 +0000

    Alignment, formatting, rename vars breaking Python syntax - use type annotations instead

--- Code/make_hash_block.py
+++ Code/make_hash_block.py
@@ -2,18 +2,20 @@ import Crypto.Hash.MD5 as MD5
 import Crypto.PublicKey.RSA as RSA
 import os
 
-def make_block(records, chain, key, nodeID):
+def make_block(records, chain, key: int, nodeID: int):
     parentBlock = chain[-1]
-    parentSignature = parentBlock[u'signature']
-    blockNumber = parentBlock[u'contents'][u'blockNumber'] + 1
-    blockContents = {u'blockNumber': blockNumber, u'parentSignature':
-        parentSignature, 'records': records
-                     u'nodeID': nodeID}
+    parentSignature = parentBlock['signature']
+    blockNumber = parentBlock['contents']['blockNumber'] + 1
+    blockContents = {'blockNumber': blockNumber,
+                     'parentSignature': parentSignature,
+                     'records': records,
+                     'nodeID': nodeID,
+                    }
     blockSignature = sign(blockContents, key)
-    block = {u'signature': blockSignature, u'contents': blockContents}
+    block = {'signature': blockSignature, 'contents': blockContents}
     return block
 
-def sign(blockContents, key, nodeID)
+def sign(blockContents, key: int, nodeID: int)
     # Signing a hash is more computationally efficient
     hash = MD5.new(blockContents).digest()
     signature = key.sign(hash, nodeID)
@@ -21,38 +23,38 @@ def sign(blockContents, key, nodeID)
 
 # This function should only be called after the decision to create a new node
 # has been approved and a node id selected
-def create_node(nodeID)
+def create_node(nodeID: int)
     # Generates a fresh public/private key pair
     key[nodeID] = RSA.alg.generate(384, os.urandom)
     pubkey[nodeID] = key[nodeID].publickey()
     return key[nodeID], pubkey[nodeID]
 
-def check_block_hash(block, pubkey[nodeID])
-    blockSignature = block[u'signature']
-    blockcontent = block[u'contents']
+def check_block_hash(block, pubKey, nodeID)
+    blockSignature = block['signature']
+    blockcontent = block['contents']
     hash = MD5.new(blockcontent).digest()
-    return pubkey[nodeID].verify(hash, blockSignature)
+    return pubKey[nodeID].verify(hash, blockSignature)
 
 def get_node_id(block)
     return nodeID
 
-def find_divegent_point(chain1[], chain2[])
+def find_divegent_point(chain1: list, chain2: list)
     return d
 
 # Chain validity should be checked before this function is called
-def pick_from_chains(chain1[], chain2[])
-    for i in (1,len(chain1[])):
+def pick_from_chains(chain1: list, chain2: list)
+    for i in (1,len(chain1)):
         nodeID[i] = get_node_id(chain1[i])
     countd1 = len(set(nodeID[]))
-    for i in (1, len(chain2[])):
+    for i in (1, len(chain2)):
         nodeID[i] = get_node_id(chain2[i])
     countd2 = len(set(nodeID[]))
     if countd1 > countd2:
-        return chain1[]
+        return chain1
     elif countd2 > countd1:
-        return chain2[]
+        return chain2
     else:
-        d = find_divegent_point(chain1[], chain2[])
+        d = find_divegent_point(chain1, chain2)
         nodeID1 = get_node_id(chain1[d+1])
         nodeID2 = get_node_id(chain2[d+1])
         n = 1
@@ -68,9 +70,9 @@ def pick_from_chains(chain1[], chain2[])
                 break
             m+=1
         if n > m:
-            return chain1[]
+            return chain1
         elif m > n:
-            return chain2[]
+            return chain2
         elif nodeID1 < nodeID2:
             return chain1[1]
         else nodeID2 =< nodeID1:
