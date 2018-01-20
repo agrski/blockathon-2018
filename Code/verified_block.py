16cacd43fa51ed79147fc44dfd7bb5bda96d92fd
Steven Ghoussain <steven.ghoussain@thehutgroup.com>
2018-01-20 19:52:52 +0000

    defined class attributes

--- Code/verified_block.py
+++ Code/verified_block.py
@@ -1,16 +1,24 @@
+from Crypto.Hash.SHA256 import SHA256Hash
+from Crypto.Signature.PKCS1_v1_5 import PKCS115_SigScheme
+
+
 class VerifiedBlock:
 
-    def __init__(self, registration_document, bureacratic_signature, tribal_signature_A, tribal_signature_B):
+    def __init__(self, registration_document, bureacratic_signature, tribal_signature_A, tribal_signature_B, blockchain):
+        self.parent_signature = blockchain[-1].get_signature()
         self.registration_document = registration_document
-        self.bureacratic_signature = bureacratic_signature
-        self.tribal_signature_A = tribal_signature_A
-        self.tribal_signature_B = tribal_signature_B
+        self._signature = (bureacratic_signature, tribal_signature_A, tribal_signature_B)
+
+        def get_signature(self):
+            return ''.join(self._signature)
 
 
 # Block contents
 
-# public key of signatories
-# name of land owner
-# GPS coordinates
-# timestamp of sign-off
-# (hash) tribal and bureaucratic signature
+# parent signature
+# registration document:
+# --- public key of signatories
+# --- name of land owner
+# --- GPS coordinates
+# --- timestamp of sign-off
+# triple of signatures
